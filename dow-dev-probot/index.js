e5066c3e6cee7bd3801280833a27fa82ab91e71a
Damaso Flores <damaso.flores@gmail.com>
2018-10-24 08:40:46 -0500

    fix(axios): Update directline posts (#3)
    - Update axios `POST` calls
    - Add better error handling

--- dow-dev-probot/index.js
+++ dow-dev-probot/index.js
@@ -4,50 +4,71 @@
  */
 
 const axios = require('axios')
-var headers = {
-  'Authorization': 'Bearer BS56HSTI0Gg.cwA.0RQ.DNUpHj8F_ogArX5rb-0FIvmiXCF88twU52ccbvCgrQU',
-  'Content-Type': 'application/json',
-  'cache-control': 'no-cache'
-}
+
+var _axios = axios.create({
+  baseURL: 'https://directline.botframework.com/v3/directline/',
+});
+
+_axios.defaults.headers.common['Authorization'] = 'Bearer BS56HSTI0Gg.cwA.0RQ.DNUpHj8F_ogArX5rb-0FIvmiXCF88twU52ccbvCgrQU'
 
 module.exports = app => {
   // When an issue is opened run the code below
   app.on('issues.opened', async context => {
     const issueComment = context.issue({ body: 'Thanks for opening this issue!' })
     // Start a conversation
-    axios({
-      method: 'post',
-      url: 'https://directline.botframework.com/v3/directline/conversations',
-      headers: {
-          'Authorization': 'Bearer BS56HSTI0Gg.cwA.0RQ.DNUpHj8F_ogArX5rb-0FIvmiXCF88twU52ccbvCgrQU',
-          'cache-control': 'no-cache'
-      }
-    })
-    .then(function (response) {
-      // Output current conversation ID
-      console.log(response.data.conversationId)
-      var data = {
-        "type": "message",
-        "from": {
-          "id": "probot"
-        },
-        "text": "hello"
-      }
-      // Build up url for sending conversation information
-      var url = 'https://directline.botframework.com/v3/directline/conversations/' + response.data.conversationId + '/activities';
-      console.log(url)
-      // Post new information for the current conversation ID
-      //axios.post(url, data, headers)
-      //  .then(function (response) {
-      //    console.log(response)
-      //  })
-      //  .catch(function (error) {
-      //    console.log(error)
-      //  })
-    })
-    .catch(function (error) {
-      console.log(error)
-    })
+
+    _axios.post('conversations')
+      .then(result => {
+        console.log('Create conversation?', result.statusText, result.data)
+        _axios.post('conversations/' + result.data.conversationId + '/activities', {
+            type: 'message',
+            from: {
+                id: 'probot'
+            },
+            text: 'issue;{ repo: "name", issueId: "56", issueTitle: "Doesnt work in IE8", issueBody: "Your app sucks" }'
+        })
+          .then(result => {
+            console.log('Send activity?', result.statusText, result.data)
+          })
+          .catch(error => {
+            if (error.response) {
+              // The request was made and the server responded with a status code
+              // that falls out of the range of 2xx
+              console.log(error.response.data);
+              console.log(error.response.status);
+              console.log(error.response.headers);
+            } else if (error.request) {
+              // The request was made but no response was received
+              // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+              // http.ClientRequest in node.js
+              console.log(error.request);
+            } else {
+              // Something happened in setting up the request that triggered an Error
+              console.log('Error', error.message);
+            }
+            console.log(error.config);
+          })
+
+      })
+      .catch(error => {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          console.log(error.response.data);
+          console.log(error.response.status);
+          console.log(error.response.headers);
+        } else if (error.request) {
+          // The request was made but no response was received
+          // `error.request` is an instance of XMLHttpRequest in the browser and an instance of
+          // http.ClientRequest in node.js
+          console.log(error.request);
+        } else {
+          // Something happened in setting up the request that triggered an Error
+          console.log('Error', error.message);
+        }
+        console.log(error.config);
+      });
+
     return context.github.issues.createComment(issueComment)
   })
 
